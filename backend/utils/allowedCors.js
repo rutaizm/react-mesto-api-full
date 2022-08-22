7e190b1de1f2b58295290b162cbd160ff26e2f98
Tatyana Izmailova <rutaizm@yandex.ru>
2022-08-22 12:59:32 +0300

    cors added

--- backend/utils/allowedCors.js
+++ backend/utils/allowedCors.js
@@ -1,9 +1,22 @@
-const allowedCors = {
-  origin: [
-    'http://rutaizm15.nomoredomains.sbs/',
-    'https://locahost:3000',
-  ],
-  credentials: true,
-};
+const allowedCors = [
+  'http://rutaizm15.nomoredomains.sbs/',
+  'https://locahost:3000',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-module.exports = allowedCors;
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      res.end();
+    }
+  }
+  next();
+};
