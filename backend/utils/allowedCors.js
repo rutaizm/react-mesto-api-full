e35ab257052e45366e81746d977aa7ff267b7b4a
Tatyana Izmailova <rutaizm@yandex.ru>
2022-08-24 12:00:57 +0300

    allowed cors changed

--- backend/utils/allowedCors.js
+++ backend/utils/allowedCors.js
@@ -3,6 +3,7 @@ const allowedCors = {
     'http://rutaizm15.nomoredomains.sbs/',
     'https://rutaizm15.nomoredomains.sbs/',
     'https://locahost:3000',
+    'http://locahost:3000',
   ],
   credentials: true,
 };
